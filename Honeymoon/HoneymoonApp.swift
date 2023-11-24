3e7f38cfa1237523835e03fe54b5492b34e847c0
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2023-11-24 21:17:58 +0900

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,20 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by SHIRAISHI HIROYUKI on 2023/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
